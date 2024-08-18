81ada7c44200b451ec1fcc3374b87d3aae0deada
Vaddiarun <116086928+Vaddiarun@users.noreply.github.com>
2024-08-19 00:39:16 +0530

    Update request.js

--- hotel/utils/request/request.js
+++ hotel/utils/request/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const configuredUrl = axios.create({
   baseURL: import.meta.env.VITE_APP_API_URL,
-  // withCredentials: true,
+  withCredentials: true,
 });
 
 export default configuredUrl;
