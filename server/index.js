eafa4ffee885d7ddd271fdd2559e93cee14f3e8f
Vaddiarun <116086928+Vaddiarun@users.noreply.github.com>
2024-08-19 00:14:57 +0530

    modified file

--- server/index.js
+++ server/index.js
@@ -20,13 +20,7 @@ app.use(cors({
     credentials: true
 }));
 
-app.options('*', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "https://hotelfrontend-ruddy.vercel.app");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.sendStatus(200);
-});
+
 app.use(express.json());
 
 app.use(cookieParser())
